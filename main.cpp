e2e40ace16150a4938cf84dcd1a8ae4d895021d9
Prooheckcp <vasco.soares.2001@gmail.com>
2025-11-23 22:16:51 +0900

    Initiate C++ Program

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    auto lang = "C++";
+    std::cout << "Hello and welcome to " << lang << "!\n";
+
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
